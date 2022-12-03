604f6c95f4eb36eddb3f71b1ae20f648d970d40a
Maxell Milay <milaymaxell@gmail.com>
2022-12-03 15:44:44 +0800

    added website logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
 import { FrontendRoute } from './enums/Routes';
-import { SET_CURRENT_USER } from './redux/reducers/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react'
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebase/auth';
 import { RootState } from './redux/store';
 import { handleAuthState } from './services/auth';
 import Redirect from './pages/Auth/components/Redirect';
